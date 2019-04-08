63a5f4898119277b16f5fc6e32453e71f1654c32
Edward Adam Payzant <payzantedwardiv@gmail.com>
2019-04-08 17:44:36 -0400

    Formatting fix

--- List.h
+++ List.h
@@ -6,28 +6,27 @@
 template <class T>
 class List
 {
-    class Node {
-        public:
-            T* data;
-            Node* next;
-            Node() {
-                data = NULL;
-                next = NULL;
-            }
-            ~Node() {
-                delete data;
-            }
-    };
-
+    private:
+        class Node {
+            public:
+                T* data;
+                Node* next;
+                Node() {
+                    data = NULL;
+                    next = NULL;
+                }
+                ~Node() {
+                    delete data;
+                }
+        };
+        Node* head;
+        Node* tail;
     public:
         List();
         ~List();
         void add(T*&);
         void print();
         List& operator+=(T*&);
-    private:
-        Node* head;
-        Node* tail;
 };
 
 #endif
